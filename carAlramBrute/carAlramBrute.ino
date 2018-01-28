45a882ba0b94bc2a9dd9a29c135a2a326e8dde50
Михаил Лямаев <misha25_live@mail.ru>
2018-01-28 20:02:30 +1000

    Set options and blink sketch

--- carAlramBrute/carAlramBrute.ino
+++ carAlramBrute/carAlramBrute.ino
@@ -6,10 +6,14 @@
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+	pinMode(13, OUTPUT);
 
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+	digitalWrite(13, 1);
+	delay(500);
+	digitalWrite(13, 0);
+	delay(500);
 }
