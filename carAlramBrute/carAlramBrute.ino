ad04284b73ab817a6991fa30e7277cda42f2c979
Михаил Лямаев <misha25_live@mail.ru>
2018-01-28 19:02:07 +1000

    Добавьте файлы проекта.

---
+++ carAlramBrute/carAlramBrute.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		carAlramBrute.ino
+ Created:	28.01.2018 19:01:59
+ Author:	misha
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
